cc2b6e7cff6b4649a1e8653c13ca00340669c60e
Roberto Calvo <KMK85464@educastur.es>
2025-09-23 06:16:41 +0200

    prueba tecnica-imagen#

--- react-prueba-tecnica/src/App.jsx
+++ react-prueba-tecnica/src/App.jsx
@@ -1,28 +1,33 @@
-import React, { useEffect, useState } from 'react'
+import React, { use, useEffect, useState } from 'react'
 
 const CAT_ENDPOINT_IMAGE_URL='https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true'
 const CAT_ENDPOINT_RANDOM_FACT='https://catfact.ninja/fact'
 export default function App() {
   const [fact,setFact]=useState("")
   const[imageUrl,setImageUrl]=useState("")
+
+  //use efect para llamar a la api
   useEffect(()=>{
     fetch(CAT_ENDPOINT_RANDOM_FACT)
     .then(res=>res.json())
     .then(data=>{
       const{fact}=data
       setFact(fact)
-      const threeFirstWord= fact.split('',4).join(' ')
-      console.log(threeFirstWord)
+      })
+  }, [])
 
+//use efect para recuperar la imagen
+useEffect(()=>{
+  if (!fact) return
+  const threeFirstWord= fact.split('',3).join(' ')
+      console.log(threeFirstWord)
       fetch(`https://cataas.com/cat/says/${threeFirstWord}?size=50&color=red&json=true`)
       .then(res=>res.json())
       .then(response=>{
         const {url}=response
         setImageUrl(url)
       })
-    })
-  },[])
-
+}, [fact])
 
   return( 
     <main>
