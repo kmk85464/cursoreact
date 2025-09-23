501659ab6968390444756a948a508449b1a8a6b1
Roberto Calvo <KMK85464@educastur.es>
2025-09-23 05:55:07 +0200

    prueba tecnica-imagen#

--- react-prueba-tecnica/src/App.jsx
+++ react-prueba-tecnica/src/App.jsx
@@ -1,21 +1,34 @@
 import React, { useEffect, useState } from 'react'
 
-//const CAT_ENDPOINT_IMAGE_URL='https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true'
+const CAT_ENDPOINT_IMAGE_URL='https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true'
 const CAT_ENDPOINT_RANDOM_FACT='https://catfact.ninja/fact'
 export default function App() {
   const [fact,setFact]=useState("")
-
+  const[imageUrl,setImageUrl]=useState("")
   useEffect(()=>{
     fetch(CAT_ENDPOINT_RANDOM_FACT)
     .then(res=>res.json())
-    .then(data=>setFact(data.fact))
+    .then(data=>{
+      const{fact}=data
+      setFact(fact)
+      const threeFirstWord= fact.split('',4).join(' ')
+      console.log(threeFirstWord)
+
+      fetch(`https://cataas.com/cat/says/${threeFirstWord}?size=50&color=red&json=true`)
+      .then(res=>res.json())
+      .then(response=>{
+        const {url}=response
+        setImageUrl(url)
+      })
+    })
   },[])
 
 
   return( 
     <main>
       <h1>App de Gatitos</h1>
-      <p>{fact}</p>
-  </main>
+      {fact&& <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt={`image extracted using three first word ${fact}`} />}
+    </main>
   )
 }
